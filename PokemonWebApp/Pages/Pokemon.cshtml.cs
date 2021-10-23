d5acbd1a643b55bb5c283924d609430ead42a2b6
augustoje <augusto.jesus@outlook.com.br>
2021-10-23 03:59:10 -0300

    Commit Consumindo api

--- PokemonWebApp/Pages/Pokemon.cshtml.cs
+++ PokemonWebApp/Pages/Pokemon.cshtml.cs
@@ -1,16 +1,36 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using PokemonWebApp.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace PokemonWebApp.Pages
 {
     public class PokemonModel : PageModel
     {
-        public void OnGet()
+
+        public List<Pokemon> Pokemons { get; private set; } = new List<Pokemon>();
+        string baseUrl = "https://localhost:44315";
+
+        public async Task OnGetAsync()
         {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("api/Pokemons");
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = response.Content.ReadAsStringAsync().Result;
+                    Pokemons = JsonConvert.DeserializeObject<List<Pokemon>>(result);
+                }
+            }
         }
     }
 }
